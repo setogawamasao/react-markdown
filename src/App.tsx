6ec2598808360e872a2936feb45cd48c9ad2c56d
masaosetogawa <ms.job.hunting@gmail.com>
2020-10-22 18:32:42 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactMarkdown from "react-markdown/with-html";
 
-function App() {
+const App = () => {
+  const markdown =
+    "# 見出し 1\n" +
+    "## 見出し 2\n" +
+    "### 見出し 3\n" +
+    "#### 見出し 4\n" +
+    "---\n" +
+    "- リスト 1\n" +
+    "- リスト 2\n" +
+    "- リスト 2-1\n" +
+    "1. 番号付きリスト 1\n" +
+    "2. 番号付きリスト 2\n" +
+    "3. 番号付きリスト 3\n" +
+    "[リンク](http://...)\n" +
+    "**強調**\n" +
+    "<h1>HTMLで表示する</h1>";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello react-markdown</h1>
+      <div>escapeHtml:falseでHTMLタグが有効となる</div>
+      <ReactMarkdown source={markdown} escapeHtml={false} />
+    </>
   );
-}
+};
 
 export default App;
